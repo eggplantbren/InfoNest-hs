96304cde031cf9df3e5561b21a7c3fa591d0c53c
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-06-24 17:16:56 +1200

    A basic generation function etc

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,31 @@
 module Main where
 
+-- Imports
+import Control.Monad.Primitive
+import qualified Data.Vector.Unboxed as U
 import Lib
+import System.Random.MWC
+import System.Random.MWC.Distributions
+
+-- An example particle type
+data MyParticle = MyParticle {
+                    myParticleMu :: Double,
+                    myParticleXs :: U.Vector Double
+                  } deriving Show
+
+-- Generate function
+myGenerate :: Gen RealWorld -> IO MyParticle
+myGenerate rng = do
+  mu <- (*10.0) <$> standard rng
+  ns <- U.replicateM 10 (standard rng)
+  let xs = U.map (+mu) ns
+  return $ MyParticle mu xs
+
+myModel :: Model MyParticle
+myModel = Model myGenerate 
 
 main :: IO ()
-main = someFunc
+main = withSystemRandom . asGenIO $ \rng -> do
+  particle <- myGenerate rng
+  print particle
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,14 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- Imports
+import Control.Monad.Primitive
+import System.Random.MWC
+
+-- a is the type of points in the (parameter x data) space
+data Model a = Model {
+
+                 -- Generate parameters and data from
+                 -- the joint prior
+                 generate :: Gen RealWorld -> IO a
+               }
+
